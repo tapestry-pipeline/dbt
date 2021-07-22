8bdddd35a1ab538e93e6db4b2fdaeb0384926a61
Katherine Beck <katherine.murphy.beck@gmail.com>
2021-07-22 15:04:25 -0700

    model for zoom and salesforce email aggregation

---
+++ models/_sources/email_model.sql
@@ -0,0 +1,14 @@
+
+with zoom as (
+    select EMAIL, FIRST_NAME, LAST_NAME
+    from {{ source('snowflake', 'TAPESTRY_WEBINAR_REGISTRANTS') }}
+),
+sf as (
+    select EMAIL, FIRSTNAME, LASTNAME
+    from {{ source('snowflake', 'TAPESTRY_CONTACT') }}
+)
+select distinct coalesce(zoom.EMAIL, sf.EMAIL) as EMAIL, 
+coalesce(zoom.FIRST_NAME, sf.FIRSTNAME) as FNAME,
+coalesce(zoom.LAST_NAME, sf.LASTNAME) as LNAME
+from zoom 
+full join sf ON sf.EMAIL=zoom.EMAIL
