1122e8ff8965f4ff068c9c8a144f29d319d3862a
Leah Garrison <lgarrison1023@gmail.com>
2021-07-27 12:19:52 -0400

    updated email_model

--- models/_sources/email_model.sql
+++ models/_sources/email_model.sql
@@ -1,15 +1,44 @@
 
+
+-- /*
+--     Welcome to your first dbt model!
+--     Did you know that you can also configure models directly within SQL files?
+--     This will override configurations stated in dbt_project.yml
+
+--     Try changing "table" to "view" below
+-- */
+
+--  {{ config(materialized='incremental',unique_key='EMAIL' ) }}
+-- {{ config(materialized='table') }}
+
 with zoom as (
+
     select EMAIL, FIRST_NAME, LAST_NAME
     from {{ source('snowflake', 'TAPESTRY_WEBINAR_REGISTRANTS') }}
+    where EMAIL is not null
+    
+
 ),
+
 sf as (
+
     select EMAIL, FIRSTNAME, LASTNAME
     from {{ source('snowflake', 'TAPESTRY_CONTACT') }}
+    where EMAIL is not null
+    
 )
+
 select distinct coalesce(zoom.EMAIL, sf.EMAIL) as EMAIL, 
 coalesce(zoom.FIRST_NAME, sf.FIRSTNAME) as FNAME,
 coalesce(zoom.LAST_NAME, sf.LASTNAME) as LNAME
-from zoom 
-where EMAIL is not null
+from zoom
 full join sf ON sf.EMAIL=zoom.EMAIL
+
+
+
+
+-- /*
+--     Uncomment the line below to remove records with null `id` values
+-- */
+
+-- -- where id is not null
